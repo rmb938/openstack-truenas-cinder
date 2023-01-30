6187ecf9510b3eea5f170d9a2f959324970714a0
Ryan Belgrave <ryan@belgrave.me>
2023-01-29 19:22:58 -0600

    log error when getting dataset

--- cinder/volume/drivers/rmb938/truenas/api/client.py
+++ cinder/volume/drivers/rmb938/truenas/api/client.py
@@ -1,8 +1,11 @@
+import logging
 from typing import Optional
 
 import requests
 from cinder.volume.drivers.rmb938.truenas.api.objects.dataset import Dataset, DatasetType
 from requests.compat import urljoin, quote_plus
+
+LOG = logging.getLogger(__name__)
 
 
 class TrueNASAPIClient(object):
@@ -17,13 +20,14 @@ class TrueNASAPIClient(object):
     def get_dataset(self, dataset_id: str) -> Optional[Dataset]:
         url = urljoin(self.__url, "/pool/dataset/id/%s" % quote_plus(dataset_id))
         resp = self.__client_session.get(url)
-        output_data = resp.json()
         if resp.status_code == 404:
             return None
         if resp.status_code != 200:
             # TODO: we probably want to wrap this with a custom exception
+            LOG.error("error getting dataset %s: %s" % (dataset_id, resp.text))
             resp.raise_for_status()
 
+        output_data = resp.json()
         if output_data['type'] == 'VOLUME':
             dataset_type = DatasetType.VOLUME
         elif output_data['type'] == 'FILESYSTEM':
